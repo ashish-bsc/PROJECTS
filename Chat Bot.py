be7ad8c05aa70806b73f1a0ea04c6bc017edfca3
Ashish Shrivastav <55585661+ashish-bsc@users.noreply.github.com>
2020-01-02 19:29:13 +0530

    Update Chat Bot.py

--- Chat Bot.py
+++ Chat Bot.py
@@ -89,7 +89,73 @@ model.fit([encoder_human_data, decoder_human_data], decoder_robot_data,
 # Save model
 model.save('bot.h5')
 
+model.load_weights('bot.h5')
+
+encoder_model = Model(encoder_humans, encoder_states)
+
+decoder_state_input_h = Input(shape=(latent_dim,))
+decoder_state_input_c = Input(shape=(latent_dim,))
+decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+decoder_robots, state_h, state_c = decoder_lstm(
+    decoder_humans, initial_state=decoder_states_inputs)
+decoder_states = [state_h, state_c]
+decoder_robots = decoder_dense(decoder_robots)
+decoder_model = Model(
+    [decoder_humans] + decoder_states_inputs,
+    [decoder_robots] + decoder_states)
+
+# Reverse-lookup token index to decode sequences back to
+# something readable.
+reverse_input_char_index = dict(
+    (i, char) for char, i in human_token_index.items())
+reverse_target_char_index = dict(
+    (i, char) for char, i in robot_token_index.items())
 
 
+def decode_sequence(input_seq):
+    # Encode the input as state vectors.
+    states_value = encoder_model.predict(input_seq)
+
+    # Generate empty target sequence of length 1.
+    target_seq = np.zeros((1, 1, num_decoder_tokens))
+    # Populate the first character of target sequence with the start character.
+    target_seq[0, 0, robot_token_index[' ']] = 1.
+
+    # Sampling loop for a batch of sequences
+    # (to simplify, here we assume a batch of size 1).
+    stop_condition = False
+    decoded_sentence = ''
+    while not stop_condition:
+        output_tokens, h, c = decoder_model.predict(
+            [target_seq] + states_value)
+
+        # Sample a token
+        sampled_token_index = np.argmax(output_tokens[0, -1, :])
+        sampled_char = reverse_target_char_index[sampled_token_index]
+        decoded_sentence += sampled_char
+
+        # Exit condition: either hit max length
+        # or find stop character.
+        if (sampled_char == '\n' or
+           len(decoded_sentence) > max_decoder_seq_length):
+            stop_condition = True
+
+        # Update the target sequence (of length 1).
+        target_seq = np.zeros((1, 1, num_decoder_tokens))
+        target_seq[0, 0, sampled_token_index] = 1.
+
+        # Update states
+        states_value = [h, c]
+
+    return decoded_sentence
 
 
+for seq_index in range(100):
+    # Take one sequence (part of the training set)
+    # for trying out decoding.
+    input_seq = encoder_human_data[seq_index: seq_index + 1]
+    decoded_sentence = decode_sequence(input_seq)
+    print('-')
+    print('Human:', input())
+    #if h1 == "start" or "START":
+    print('Bot:', decoded_sentence)
